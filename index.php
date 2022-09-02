4375bfe7f051ee56a14bc6a8900051581b60e343
Adeilsoara <adeilsonaragao@hotmail.com>
2022-09-02 14:34:30 -0300

    Meu primeiro commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+</head>
+<body>
+    <div>Não consigo ler nada</div>
+    <div>Não consigo ler nada</div>
+    <div>Não consigo ler nada</div>
+    <div>Não consigo ler nada</div>
+    <div>Não consigo ler nada</div>
+    <div class="teste"></div>
+    <div id="teste"></div>
+    <!-- Comentário -->
+</body>
+</html>
